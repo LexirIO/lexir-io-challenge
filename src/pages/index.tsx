9c54b90b87949e6845dbc5a004fd9243f96c62e6
Jean-Pierre Carvalho <hello@jeanpierrecarvalho.pt>
2022-08-29 16:03:35 +0100

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Lexir Frontend Assessment!</title>
+        <meta name="description" content="Lexir Frontend Assessment!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Lexir Frontend Assessment!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{" "}
+          <code className={styles.code}>pages/index.tsx</code>
+        </p>
+      </main>
+    </div>
+  );
+}
