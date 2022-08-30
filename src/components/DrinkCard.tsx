96a0ba5f04c60d584cb4a5745fc29e87a114c919
Leonardo Araujo <leoujooficial@gmail.com>
2022-08-29 22:31:40 -0300

    Navbar and basic components creation

---
+++ src/components/DrinkCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DrinkCard = () => {
+  return (
+    <div>DrinkCard</div>
+  )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg shadow-md py-5 bg-white relative flex items-center w-full justify-between">
+      <div className="px-6 w-full flex flex-wrap items-end justify-between">
+        <img src="/assets/img/lexir-logo-black.svg" className="m-2" />
+
+        <div className="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
+          <ul className="navbar-nav flex justify-end">
+            <li className="nav-item">
+              <a
+                className="nav-link block pr-2 lg:px-10 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                href="#!"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+              >
+                PRODUCTS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link block pr-2 lg:px-10 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                href="#!"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+              >
+                BRANDS
+              </a>
+            </li>
+            <li className="nav-item mb-2 flex">
+              <img src="/assets/icons/user-avatar.svg" width="30px" />
+              <a
+                className="nav-link pl-3 pr-10 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out flex align-middle"
+                href="#!"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+              >
+                HELLO, LEANDRO
+              </a>
+            </li>
+            <li className="nav-item mb-2 flex">
+              <img src="/assets/icons/basket-add.svg" width="30px" />
+              <a
+                className="nav-link pl-3 pr-10 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                href="#!"
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+              >
+                CART
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
