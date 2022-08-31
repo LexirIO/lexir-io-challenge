9e69dcecbc4f7dfaf780af1c832e42629470cea8
Nico <nicoc12024@gmail.com>
2022-08-31 15:46:16 +0200

    m

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,4 @@
 import "../styles/globals.css";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: any) {
   return <Component {...pageProps} />;
