af30ff37a8f41f3da493b5f48b78974edc21240f
yellow_may <yellow.may.21@gmail.com>
2022-09-02 16:03:11 +0100

    custom styles variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,33 @@ module.exports = {
 		'./src/components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			white: '#FFFFFF',
+			black: '#000000',
+			primary: {
+				DEFAULT: '#2D2D2D',
+			},
+			secondary: {
+				DEFAULT: '#8F8F8F',
+				light: '#F2F2F2',
+			},
+			grey: {
+				100: '#F9F9F9',
+				200: '#CACACA',
+				300: '#CCCCCC',
+			},
+			success: {
+				DEFAULT: '#1C8C64',
+			},
+		},
+		fontFamily: {
+			lato: ['Lato', 'sans-serif'],
+			'source-serif-pro': ['Source Serif Pro', 'serif'],
+		},
+		boxShadow: {
+			DEFAULT: '0px 4px 6px rgba(0, 0, 0, 0.06)',
+		},
 		extend: {},
 	},
 	plugins: [],
