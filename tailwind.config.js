112eac0d52cc8459f7723836fc83d30dace3b57c
efdalyalcin <efdalyalcin@hotmail.com>
2022-09-04 22:25:19 +0300

    tailwind config eddited

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
         dots: '#8F8F8F',
         lightGray: '#CACACA',
         lightGray1: '#F9F9F9',
+        success: '#1C8C64',
       },
       fontFamily: {
         regular: ['Lato'],
@@ -24,6 +25,9 @@ module.exports = {
       },
       gridTemplateColumns: {
         twoToOne: '2fr 1fr',
+      },
+      lineHeight: {
+        extra: '1.17',
       }
     },
   },
