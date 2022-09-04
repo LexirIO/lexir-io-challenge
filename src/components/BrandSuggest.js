01c8be4f07d474bfa340e1399cbdbb0dbbde2c1c
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-09-04 16:53:21 +0100

    brand suggestion

--- src/components/BrandSuggest.js
+++ src/components/BrandSuggest.js
@@ -6,8 +6,8 @@ const BrandSuggest = () => {
     return (
         <div className="flex justify-center w-[1280] h-[392px] ">
             <div className="flex w-[1280px] h-[392px] mx-[33px]  ">
-                <div className="flex  flex-col w-[606px] h-[392px]  ">
-                    <div className="flex flex-col w-[556px] h-[216px] mt-[50px] mr-[15px] mb-[28px] ml-[34px] bg-white  ">
+                <div className="flex  flex-col w-[606px] h-[392px] bg-rose-100  ">
+                    <div className="  flex flex-col w-[556px] h-[216px] mt-[50px] mr-[15px] mb-[28px] ml-[34px]  ">
                         <div>
                             <Image src={BrandTitle} alt="suggestion title" />
                         </div>
@@ -15,8 +15,8 @@ const BrandSuggest = () => {
                             <p className="mt-[23px]">
                                 Lewir is always looking to connect with new
                                 wxciting craft brands from around the world. If
-                                you have a brand in mind that you'd like to see
-                                on Lexir, let us know.
+                                you have a brand in mind that you would like to
+                                see on Lexir, let us know.
                             </p>
                         </div>
                     </div>
