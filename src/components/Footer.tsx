b045a6622b61d634c3a83429fdeb14848f9a00f1
traj3ctory <davidbenard.bd@gmail.comy>
2022-09-04 05:50:36 +0100

    feat: create the layout structure for the brands page

---
+++ src/components/Footer.tsx
@@ -0,0 +1,152 @@
+import { BsInstagram, BsLinkedin, BsFacebook, BsSnapchat } from "react-icons/bs";
+
+function Footer() {
+  return (
+    <footer className="px-12 py-8 bg-[#2D2D2D] text-white">
+      <div className="max-w-screen-xl mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-4">
+          <div className="mb-6">
+            <a href="#" className="flex items-center">
+              <span className="self-center text-2xl font-semibold">LEXIR</span>
+            </a>
+          </div>
+          <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-400 uppercase">
+              Community
+            </h2>
+            <ul className="text-white">
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Lexir for Brands
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Business Buyers
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Sales Afiliates
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-400 uppercase">
+              Platform
+            </h2>
+            <ul className="text-white">
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Resources
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Pricing
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Get Started
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-400 uppercase">
+              Company
+            </h2>
+            <ul className="text-white">
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  About
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Contact
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Legal
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="mb-6 text-sm font-semibold text-gray-400 uppercase">
+              Lexir Shop
+            </h2>
+            <ul className="text-white">
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Brands
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Spirits
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Wine
+                </a>
+              </li>
+              <li className="mb-2">
+                <a href="#" className="hover:underline">
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul className="text-white flex text-gray-400">
+              <li className="mb-4ml-3">
+                <a href="#" className="hover:underline">
+                  <BsInstagram className="mr-2" />
+                </a>
+              </li>
+              <li className="mb-4 ml-3">
+                <a href="#" className="hover:underline">
+                  <BsFacebook className="mr-2" />
+                </a>
+              </li>
+              <li className="mb-4 ml-3">
+                <a href="#" className="hover:underline">
+                  <BsLinkedin className="mr-2" />
+                </a>
+              </li>
+              <li className="mb-4 ml-3">
+                <a href="#" className="hover:underline">
+                  <BsSnapchat className="mr-2" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-200"></hr>
+        <div className="flex justify-between">
+          <span className=" text-gray-500">
+            © 2022{" "}
+            <a href="#" className="hover:underline">
+              Lexir Inc.
+            </a>
+          </span>
+          <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+            <a href="#" className="text-white hover:text-gray-400">
+              Privacy
+            </a>
+            <a href="#" className="text-white hover:text-gray-400">
+              Terms of Service
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,44 @@
+// import Head from "next/head";
+// import styles from "../styles/Home.module.css";
+
+// export default function Home() {
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Lexir Frontend Assessment!</title>
+//         <meta name="description" content="Lexir Frontend Assessment!" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+
+//       <main className={styles.main}>
+//         <h1 className={styles.title}>
+//           Welcome to <a href="https://nextjs.org">Lexir Frontend Assessment!</a>
+//         </h1>
+
+//         <p className={styles.description}>
+//           Get started by editing{" "}
+//           <code className={styles.code}>pages/index.tsx</code>
+//         </p>
+//       </main>
+//     </div>
+//   );
+// }
+
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Layout from "@components/Layout";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Lexir Frontend Assessment!</title>
-        <meta name="description" content="Lexir Frontend Assessment!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Lexir Frontend Assessment!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-      </main>
-    </div>
+    <Layout
+      head={
+        <Head>
+          <title>Lexir Frontend Assessment!</title>
+          <meta name="description" content="Lexir Frontend Assessment!" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+      }
+    >
+      <div>Hello World</div>
+    </Layout>
   );
 }
