b8350f269fc80dc04171048e9432316a7ef8a3bc
Eshy10 <adewalebose15@gmail.com>
2022-09-07 01:41:39 +0100

    chore: add header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Image from "next/image";
+import HomeIcon from '@assets/house.svg';
+import ArrowIcon from "@assets/arrow-right.svg"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className='flex justify-between w-[50%] xl:w-[40%]'>
+    <Image src={HomeIcon} alt="dotted icon" className="cursor-pointer"/> 
+    <p className='text-[#2D2D2D]'>Home</p>
+    <Image src={ArrowIcon} alt="dotted icon" className="cursor-pointer"/> 
+    <p className='text-[#2D2D2D]'>Spirits</p>  
+    <Image src={ArrowIcon} alt="dotted icon" className="cursor-pointer"/> 
+    <p className='text-[#2D2D2D]'>Kiss My</p>  
+    <Image src={ArrowIcon} alt="dotted icon" className="cursor-pointer"/> 
+    <p className='text-[#2D2D2D] font-bold'>Kiss My Rhubarb</p>  
+    </section>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import Navbar from "@components/navbar/Navbar";
 import Footer from "@components/footer/Footer";
 import Brand from "@components/brand/Brand";
 import Product from "@components/product";
+import Header from "@components/header/Header";
 
 export default function Home() {
     return (
         <main className=" bg-white 2xl:w-[60%] 2xl:m-auto 2xl:bg-[#ffffff]">
             <Navbar />
             <Section>
+                <Header />
                 <Product />
                 <Brand />
             </Section>
