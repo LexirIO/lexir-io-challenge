51542b25193c1fc8d8bc4e6462b667e4a438f4ae
Victor Opara <opara.victor@gmail.com>
2022-09-07 19:16:57 +0100

    Configurations commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "metal-dark": "#2D2D2D",
+        "text-grey": "#8F8F8F",
+      },
+
+    },
+  },
+  plugins: [],
+};
+
+// C:\Users\User\desktop\project-starter\lexir-io-challenge\src\pages
