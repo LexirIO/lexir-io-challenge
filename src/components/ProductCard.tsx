24af6fbce565fe9f665b996f30b5856031511c79
Grigorios Paschalidis <gregpasch8@gmail.com>
2022-09-08 14:37:23 +0300

    All done :)

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -9,7 +9,6 @@ export default function ProductImage() {
           <Image
             src={Rhubharb}
             alt = ''
-            objectFit="cover"
             width={748}
             height={488}
           />
